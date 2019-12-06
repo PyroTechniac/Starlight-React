d23aea27e9939c84627d93003ade311b5ca6fd40
Gryffon Bellish <owenbellish@gmail.com>
2019-12-06 13:58:09 -0500

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const useStyles = makeStyles(theme => ({
+	root: {
+		padding: theme.spacing(3, 2)
+	}
+}));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function PaperSheet(): JSX.Element {
+	const classes = useStyles();
+	return (
+		<Paper className={classes.root}>
+			<Typography variant="h5" component="h3">
+                This is a sheet of paper.
+			</Typography>
+			<Typography component="p">
+                Paper can be used to build surface or other elements for your application.
+			</Typography>
+		</Paper>
+	);
+}
+
+ReactDOM.render(<PaperSheet />, document.getElementById('root'));
